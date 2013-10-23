640880c0159f13283ccca385684ee5704e8ed827
Daniel Grant <danieljohngrant@gmail.com>
2013-10-23 12:52:20 +0100

    Extend jquery ui datepicker to add defaults and prevent responsive screen from showing. Add css animations to tranistion in screen

---
+++ js/jquery-ui.responsive-datepicker.js
@@ -0,0 +1,25 @@
+(function() {
+
+  var isTouch = 'ontouchstart' in window || 'onmsgesturechange' in window;
+  var _datepicker = $.fn.datepicker;
+
+  $.fn.datepicker = function(options) {
+    var datepickerId = '#ui-datepicker-div';
+    var defaults = {
+      showAnim: '',
+      beforeShow: function() {
+        $(datepickerId).addClass('is-open');
+      },
+      onClose: function() {
+        $(datepickerId).removeClass('is-open');
+      },
+      showButtonPanel: true,
+      closeText: "Cancel"
+    }
+    options = $.extend({}, options, defaults);
+    _datepicker.apply(this, [options]);
+
+    $(datepickerId).hide();
+  }
+
+})();
